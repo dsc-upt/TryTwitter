a4542801ffddd1e27c8349ca50541162899afa7e
Steven Billich <stevenbillich17@gmail.com>
2021-12-02 21:43:46 +0200

    ModificareInterfata

---
+++ bussiness/src/main/java/com/example/bussiness/User.kt
@@ -0,0 +1,6 @@
+package com.example.bussiness
+
+class User (nume: String, parola: String) {
+    val nume: String = nume
+    val parola: String = parola
+}
---
+++ bussiness/src/main/java/com/example/bussiness/UserMapper.kt
@@ -0,0 +1,13 @@
+package com.example.bussiness
+
+class UserMapper {
+    companion object {
+        fun getUsersList(): List<User> {
+            val usersList = mutableListOf<User>()
+            for (i in 0..5) {
+                usersList.add(User("admin","admin"))
+            }
+            return usersList
+        }
+    }
+}
